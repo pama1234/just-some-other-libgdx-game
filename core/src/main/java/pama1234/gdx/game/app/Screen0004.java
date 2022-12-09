bd271df17667ed1a9a7086b16e48788be797fffb
Pama1234 <pama1234@163.com>
2022-12-09 10:09:26 +0800

    clean

--- core/src/main/java/pama1234/gdx/game/app/Screen0004.java
+++ core/src/main/java/pama1234/gdx/game/app/Screen0004.java
@@ -25,13 +25,9 @@ public class Screen0004 extends ScreenCore3D{
     cam.point.set(0,0,-320);
     noStroke();
     // font.getData().markupEnabled=true;
-    // backgroundColor(0);
-    // println(width,height);
     stage=new Stage(viewport=new ScalingViewport(Scaling.fit,width,height,screenCam),imageBatch);
     font.load(0);
-    // Gdx.input.setOnscreenKeyboardVisible(false,OnscreenKeyboardType.Default);
     tfs=new TextFieldStyle(font,color(0),
-      // tfs=new TextFieldStyle(font.data[0],color(0),
       new DrawableEntity(this,(batch,x,y,w,h)-> {
         beginBlend();
         fill(0,191);
@@ -45,10 +41,8 @@ public class Screen0004 extends ScreenCore3D{
       new DrawableEntity(this,(batch,x,y,w,h)-> {
         fill(221,244,196);
         rect(x,y,w,h);
-        // rect(0,0,width,height);
       }));
     textField=new TextField("1234",tfs);
-    // System.out.println(textField.isPasswordMode());
     textField.setPosition(u,u);
     textField.setOnscreenKeyboard(new OnscreenKeyboard() {
       @Override
@@ -57,31 +51,13 @@ public class Screen0004 extends ScreenCore3D{
       }
     });
     textField.setAlignment(Align.left);
-    // {
-    //   @Override
-    //   public boolean fire(Event event) {
-    //     if(event instanceof InputEvent e) {
-    //       println(e,e.getStageX(),e.getStageY());
-    //       println(mouse.x,mouse.y);
-    //       // e.setStageX(mouse.x);
-    //       // e.setStageY(mouse.y);
-    //     }else {
-    //       println(event);
-    //     }
-    //     return super.fire(event);
-    //   }
-    // };
     stage.addActor(textField);
     inputProcessor.sub.add.add(stage);
-    // inputProcessor.sub.add.add(textField);
-    // textField.fire(null);
     centerCam.add.add(new EntityListener() {
       @Override
       public void display() {
         fill(0);
-        // rect(-u*2,-u*2,u*4,u*4);
         rect(-256,-256,512,512);
-        // System.out.println(123);
       }
     });
     centerScreen.add.add(new ConfigInfo(this));
@@ -94,10 +70,6 @@ public class Screen0004 extends ScreenCore3D{
   @Override
   public void display() {
     stage.draw();
-    // imageBatch.begin();
-    // textField.draw(imageBatch,1);
-    // imageBatch.end();
-    // rect(mouse.x,mouse.y,u,u);
   }
   @Override
   public void frameResized() {
---
+++ core/src/main/java/pama1234/gdx/game/app/Screen0005.java
@@ -0,0 +1,12 @@
+package pama1234.gdx.game.app;
+
+public class Screen0005 extends ScreenCore3D{
+  @Override
+  public void setup() {}
+  @Override
+  public void update() {}
+  @Override
+  public void display() {}
+  @Override
+  public void frameResized() {}
+}
