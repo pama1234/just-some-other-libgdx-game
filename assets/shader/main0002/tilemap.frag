d7e860d5c0988b4cdaa91cf040f3973e6101b430
Pama1234 <pama1234@163.com>
2023-01-22 21:27:26 +0800

    update

--- assets/shader/main0002/tilemap.frag
+++ assets/shader/main0002/tilemap.frag
@@ -11,18 +11,28 @@ varying LOWP vec4 v_color;
 varying HIGHP vec2 v_texCoords;
 uniform sampler2D u_texture;
 
+uniform vec2 resolution;
+
 uniform sampler2D tiles;
 uniform sampler2D tilesData;
 
-uniform vec2 resolution;
+uniform vec2 tileSize;
+
+vec2 flip(vec2 tIn) {
+  return vec2(tIn.x,1.0-tIn.y);
+}
 
 void main() {
-  // gl_FragColor=vec4(
-  //   v_texCoords.x*resolution.x/255.0,
-  //   v_texCoords.y*resolution.y/255.0,
-  //   0.0,1.0);
-  gl_FragColor=vec4(v_texCoords.x,v_texCoords.y,0.0,1.0);
-  // gl_FragColor=v_color*texture2D(tiles,v_texCoords);
-  // gl_FragColor=v_color*texture2D(tilesData,v_texCoords);
-  // gl_FragColor=v_color*texture2D(tiles,v_texCoords)*texture2D(tilesData,v_texCoords);
+  // gl_FragColor=vec4(v_texCoords.x,v_texCoords.y,0.0,1.0);
+  // gl_FragColor=v_color;
+  // gl_FragColor=texture2D(tiles,v_texCoords);
+  // gl_FragColor=texture2D(tiles,v_texCoords);
+  // gl_FragColor=texture2D(tilesData,v_texCoords);
+  vec4 tileDataCell=texture2D(tilesData,v_texCoords);
+  // tileDataCell.xy*=resolution;
+  // tileDataCell.xy*=tileSize;
+  // vec4 tileDataCell=flip(texture2D(tilesData,v_texCoords));
+  gl_FragColor=texture2D(tiles,mod(v_texCoords,tileDataCell.xy));
+  // gl_FragColor=texture2D(tiles,flip(mod(v_texCoords,tileDataCell.xy)));
+  // gl_FragColor=texture2D(tiles,v_texCoords)*texture2D(tilesData,v_texCoords);
 }
--- core/src/main/java/pama1234/gdx/game/app/app0002/Screen0015.java
+++ core/src/main/java/pama1234/gdx/game/app/app0002/Screen0015.java
@@ -3,9 +3,11 @@ package pama1234.gdx.game.app.app0002;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.VertexAttribute;
 import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 import pama1234.gdx.util.app.ScreenCore3D;
@@ -14,40 +16,39 @@ import pama1234.gdx.util.listener.EntityListener;
 
 public class Screen0015 extends ScreenCore3D{
   public Graphics tilesData;
+  // public Texture tilesData;
+  // public Pixmap pixmap;
   public ShaderProgram shader;
   public float[] idata=new float[8];
   public Texture tiles;
   public Mesh mesh;
   public float[] verts=new float[30];
-  // public int texturePos=GL30.GL_MAX_TEXTURE_UNITS-1;
   public EntityListener shaderTester=new EntityListener() {
     @Override
     public void display() {
       Gdx.gl20.glClear(GL30.GL_COLOR_BUFFER_BIT);
       Gdx.gl20.glEnable(GL30.GL_TEXTURE_2D);
       beginBlend();
-      // System.out.println(123);
-      shaderUpdate();
       mesh.render(shader,GL30.GL_TRIANGLES);
+      // Gdx.gl20.glClear(GL30.GL_COLOR_BUFFER_BIT);
     }
   };
   public void shaderUpdate() {
-    // Gdx.gl.glActiveTexture(GL30.GL_TEXTURE1);
-    // Gdx.gl.glBindTexture(tiles.glTarget,GL30.GL_TEXTURE1);
     tiles.bind(1);
     tilesData.texture.bind(2);
     shader.bind();
-    shader.setUniformMatrix("u_projTrans",imageBatch.getProjectionMatrix());
-    // shader.setUniformMatrix("u_projTrans",cam.camera.projection);
+    shader.setUniformMatrix("u_projTrans",screenCam.combined);
+    // shader.setUniformf("v_color",1,1,1,1);
+    shader.setUniformf("tileSize",18f/tiles.getWidth(),18f/tiles.getHeight());
     shader.setUniformi("tiles",1);
     shader.setUniformi("tilesData",2);
   }
   @Override
   public void setup() {
-    background(0);
+    backgroundColor(0);
     // createAndPrintDefaultShader();
     noStroke();
-    background=false;
+    // background=false;
     tiles=new Texture(Gdx.files.internal("image/tiles.png"));
     ShaderProgram.pedantic=false;
     shader=new ShaderProgram(
@@ -60,6 +61,9 @@ public class Screen0015 extends ScreenCore3D{
     updateMesh();
     // font.load(0);
     tilesData=new Graphics(this,32,32);
+    // pixmap=tilesData.texture.getTextureData().consumePixmap();
+    // tilesData=new Texture(32,32,Format.RGBA8888);
+    // pixmap=tilesData.getTextureData().consumePixmap();
     centerScreen.add.add(shaderTester);
   }
   public void printLog(String in) {
@@ -69,13 +73,18 @@ public class Screen0015 extends ScreenCore3D{
   public void update() {
     // updateMesh();
     updateTilesData();
-    // shaderUpdate();
+    shaderUpdate();
   }
   public void updateTilesData() {
     tilesData.beginDraw();
-    fill(frameCount%256);
-    rect(8,8,8,8);
+    background(frameCount%256,frameCount%256,0);
+    fill(mouse.ox,mouse.oy,0);
+    rect(8,8,16,16);
     tilesData.endDraw();
+    // pixmap.drawPixel(0,0,tileToColor(mouse.ox,mouse.oy));
+  }
+  public int tileToColor(int x,int y) {
+    return (x<<24)|(y<<16);
   }
   @Override
   public void displayWithCam() {}
