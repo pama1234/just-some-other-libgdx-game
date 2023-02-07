a1cb3630ad62fca950941aeac2933b054dd3ed23
Pama1234 <pama1234@163.com>
2023-02-07 14:52:21 +0800

    update

--- core/src/main/java/pama1234/gdx/game/state/state0001/game/entity/util/MovementLimitBox.java
+++ core/src/main/java/pama1234/gdx/game/state/state0001/game/entity/util/MovementLimitBox.java
@@ -9,6 +9,10 @@ public class MovementLimitBox extends OuterBox{
   public boolean inAir;
   public float floor,leftWall,rightWall,ceiling;
   public RectF rectConst;
+  //---
+  public boolean leftDown,leftUp,rightDown,rightUp;
+  // public boolean xFlag,yFlag;
+  public int desX1,desY1,desX2,desY2;
   public MovementLimitBox(LivingEntity p) {
     super(p);
     rectConst=new RectF(
@@ -39,15 +43,28 @@ public class MovementLimitBox extends OuterBox{
   public void doInAirTest() {
     inAir=p.point.pos.y<floor;
   }
+  @Override
+  public void update() {
+    super.update();
+    //--------------
+    float tf=(p.point.f-1)*p.point.step;
+    float dx=p.point.vel.x*tf,
+      dy=p.point.vel.y*tf;
+    float tx=p.x()+p.type.dx+dx,
+      ty=p.y()+p.type.dy+dy;
+    desX1=p.xToBlockCord(tx);
+    desY1=p.xToBlockCord(ty);
+    desX2=p.xToBlockCord(tx+p.type.w-0.01f);
+    desY2=p.xToBlockCord(ty+p.type.h-0.01f);
+    leftUp=x1!=desX1&&y1!=desY1;
+    leftDown=x1!=desX1&&y2!=desY2;
+    rightUp=x2!=desX2&&y1!=desY1;
+    rightDown=x2!=desX2&&y2!=desY2;
+    if(leftUp||leftDown||rightUp||rightDown) p.p.println(leftUp,leftDown,rightUp,rightDown);
+  }
   public void updateLimit() {
     int blockWidth=p.pw.settings.blockWidth;
     int blockHeight=p.pw.settings.blockHeight;
-    // bugFix();
-    // if(p instanceof MainPlayer) System.out.println(w+" "+h);
-    // if(p instanceof MainPlayer) {
-    //   if(xFlag) System.out.println("xFlag "+x1+" "+desX);
-    //   if(yFlag) System.out.println("yFlag "+y1+" "+desY);
-    // }
     if(testCeiling()) doCeiling(blockHeight);
     else ceiling=(y1-4)*blockHeight;
     if(testFloor()) doFloor(blockHeight);
@@ -56,32 +73,37 @@ public class MovementLimitBox extends OuterBox{
     else leftWall=(x1-4)*blockWidth;
     if(testRight()) doRight(blockWidth);
     else rightWall=(x2+4)*blockWidth;
-    if(xFlag&&yFlag) {
-      System.out.println("MovementLimitBox.updateLimit()");
-      boolean flag=UtilMath.abs(p.point.vel.y)>UtilMath.abs(p.point.vel.x);
-      if(p.point.vel.x>0) {//右
-        if(p.point.vel.y>0) {//右下
-          if(!Block.isNotFullBlock(p.getBlock(x2+1,y2+1))) {
-            if(flag) doRight(blockWidth);
-            else doFloor(blockHeight);
-          }
-        }else {//右上
-          if(!Block.isNotFullBlock(p.getBlock(x2+1,y1-1))) {
-            if(flag) doRight(blockWidth);
-            else doCeiling(blockHeight);
-          }
+    // System.out.println("MovementLimitBox.updateLimit()");
+    cornerFix(blockWidth,blockHeight);
+  }
+  public void cornerFix(int blockWidth,int blockHeight) {
+    boolean flag=UtilMath.abs(p.point.vel.y)>UtilMath.abs(p.point.vel.x);
+    if(p.point.vel.x>0) {//右
+      if(p.point.vel.y>0) {//右下
+        if(rightDown&&!Block.isNotFullBlock(p.getBlock(desX2,desY2))) {
+          System.out.println("右下");
+          if(flag) doRight(blockWidth);
+          else doFloor(blockHeight);
         }
-      }else {//左
-        if(p.point.vel.y>0) {//左下
-          if(!Block.isNotFullBlock(p.getBlock(x1-1,y2+1))) {
-            if(flag) doLeft(blockWidth);
-            else doFloor(blockHeight);
-          }
-        }else {//左上
-          if(!Block.isNotFullBlock(p.getBlock(x1-1,y1-1))) {
-            if(flag) doLeft(blockWidth);
-            else doCeiling(blockHeight);
-          }
+      }else {//右上
+        if(rightUp&&!Block.isNotFullBlock(p.getBlock(desX2,desY1))) {
+          System.out.println("右上");
+          if(flag) doRight(blockWidth);
+          else doCeiling(blockHeight);
+        }
+      }
+    }else {//左
+      if(p.point.vel.y>0) {//左下
+        if(leftDown&&!Block.isNotFullBlock(p.getBlock(desX1,desY2))) {
+          System.out.println("左下");
+          if(flag) doLeft(blockWidth);
+          else doFloor(blockHeight);
+        }
+      }else {//左上
+        if(leftUp&&!Block.isNotFullBlock(p.getBlock(desX1,desY1))) {
+          System.out.println("左下");
+          if(flag) doLeft(blockWidth);
+          else doCeiling(blockHeight);
         }
       }
     }
--- core/src/main/java/pama1234/gdx/game/state/state0001/game/entity/util/OuterBox.java
+++ core/src/main/java/pama1234/gdx/game/state/state0001/game/entity/util/OuterBox.java
@@ -5,8 +5,6 @@ import pama1234.gdx.game.state.state0001.game.entity.LivingEntity;
 public class OuterBox{
   public LivingEntity p;
   public int x1,y1,x2,y2,w,h;
-  public boolean xFlag,yFlag;
-  public int desX,desY;
   public OuterBox(LivingEntity p) {
     this.p=p;
   }
@@ -17,14 +15,6 @@ public class OuterBox{
     y2=p.blockY2();
     w=x2-x1;
     h=y2-y1;
-    //--------------
-    float tf=(p.point.f-1)*p.point.step;
-    // xFlag=x1!=p.xToBlockCord(p.x()+p.type.dx+p.point.vel.x*tf);
-    // xFlag=y1!=p.xToBlockCord(p.y()+p.type.dy+p.point.vel.y*tf);
-    xFlag=x1!=(desX=p.xToBlockCord(p.x()+p.type.dx+p.point.vel.x*tf));
-    xFlag=y1!=(desY=p.xToBlockCord(p.y()+p.type.dy+p.point.vel.y*tf));
-    // desX=p.xToBlockCord(p.x()+p.type.dx+p.point.vel.x*tf);
-    // desY=p.xToBlockCord(p.y()+p.type.dy+p.point.vel.y*tf);
   }
   @Override
   public String toString() {
