386b12dc5575f2af95ead09fb6f23dd5f3a5c995
Pama1234 <pama1234@163.com>
2023-02-08 17:04:58 +0800

    update

--- core/src/main/java/pama1234/gdx/game/state/state0001/Debug.java
+++ core/src/main/java/pama1234/gdx/game/state/state0001/Debug.java
@@ -10,6 +10,7 @@ import pama1234.gdx.game.state.state0001.game.player.PlayerController;
 import pama1234.gdx.game.state.state0001.game.world.World0001;
 import pama1234.gdx.game.ui.generator.UiGenerator;
 import pama1234.gdx.game.ui.util.Button;
+import pama1234.math.UtilMath;
 
 public class Debug extends StateEntity0001{
   public Button<?>[] buttons;
@@ -23,9 +24,11 @@ public class Debug extends StateEntity0001{
     world=new World0001(p,null);
     world.dataDir="data/saved/debug-world";
     world.settings.g=0;
-    player=new MainPlayer(p,world,0,0);
+    // player=new MainPlayer(p,world,0,0);
+    player=world.yourself;
     player.inventory=new Inventory(player,3,1);
-    controller=new PlayerController(p,player);
+    // controller=new PlayerController(p,player);
+    controller=player.ctrl;
   }
   @Override
   public void from(State0001 in) {
@@ -37,16 +40,61 @@ public class Debug extends StateEntity0001{
   }
   @Override
   public void keyPressed(char key,int keyCode) {
-    if(keyCode==Keys.Z) p.state(State0001.Settings);
+    if(keyCode==Keys.X) p.state(State0001.Settings);
+    else if(keyCode==Keys.Z) {
+      player.update();
+      p.cam2d.point.des.set(player.blockX1()*world.blockWidth(),player.blockY1()*world.blockHeight(),0);
+      // p.cam2d.point.des.set((player.blockX1()+player.blockX2()+1)/2f*world.blockWidth(),(player.blockY1()+player.blockY2())/2f*world.blockHeight(),0);
+      p.cam2d.point.pos.set(p.cam2d.point.des);
+    }
   }
   @Override
   public void update() {
-    player.update();
+    if(p.keyPressedArray.contains(Keys.LEFT)) player.point.vel.x-=controller.speed;
+    if(p.keyPressedArray.contains(Keys.RIGHT)) player.point.vel.x+=controller.speed;
+    if(p.keyPressedArray.contains(Keys.UP)) player.point.vel.y-=controller.speed;
+    if(p.keyPressedArray.contains(Keys.DOWN)) player.point.vel.y+=controller.speed;
+    if(p.keyPressedArray.contains(Keys.C)) player.update();
+    p.cam2d.point.des.set(player.blockX1()*world.blockWidth(),player.blockY1()*world.blockHeight(),0);
+    // p.cam2d.point.des.set((player.blockX1()+player.blockX2()+1)/2f*world.blockWidth(),(player.blockY1()+player.blockY2())/2f*world.blockHeight(),0);
+    p.cam2d.point.pos.set(p.cam2d.point.des);
+    // p.println(player.blockX1());
+    // p.println(controller.limitBox.p.blockX1());
+    // System.out.println(world.xToBlockCord(player.x1()));
+    // p.println(controller.limitBox.x1);
   }
   @Override
   public void display() {}
   @Override
   public void displayCam() {
-    Game.drawLimitBox(p,18,18,controller.limitBox);
+    p.beginBlend();
+    Game.drawLimitBox(p,world.settings.blockWidth,world.settings.blockHeight,controller.limitBox);
+    float tx,ty;
+    p.doStroke();
+    p.stroke(255,127);
+    tx=player.x1();
+    ty=player.y1();
+    float dx=player.point.dx();
+    float dy=player.point.dy();
+    float mag=UtilMath.mag(dx,dy);
+    float mx=dx*18/mag;
+    float my=dy*18/mag;
+    p.line(tx,ty,tx+dx,ty+dy);
+    p.line(tx,ty,tx+mx,ty+my);
+    tx=player.x1();
+    ty=player.y2();
+    p.line(tx,ty,tx+dx,ty+dy);
+    p.line(tx,ty,tx+mx,ty+my);
+    tx=player.x2();
+    ty=player.y1();
+    p.line(tx,ty,tx+dx,ty+dy);
+    p.line(tx,ty,tx+mx,ty+my);
+    tx=player.x2();
+    ty=player.y2();
+    p.line(tx,ty,tx+dx,ty+dy);
+    p.line(tx,ty,tx+mx,ty+my);
+    p.noStroke();
+    // System.out.println(controller.limitBox.x1);
+    p.endBlend();
   }
 }
