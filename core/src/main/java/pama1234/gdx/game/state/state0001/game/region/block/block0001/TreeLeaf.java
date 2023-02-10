0f26de6eb692eb49d1876d1220c28cb04379a857
Pama1234 <pama1234@163.com>
2023-02-10 20:54:28 +0800

    update

--- core/src/main/java/pama1234/gdx/game/state/state0001/game/region/block/block0001/TreeLeaf.java
+++ core/src/main/java/pama1234/gdx/game/state/state0001/game/region/block/block0001/TreeLeaf.java
@@ -74,15 +74,15 @@ public class TreeLeaf extends MetaBlock{
   }
   @Override
   public void initBlock(Block in) {
-    if(in.intData==null||in.intData.length<2) in.intData=new int[] {1,1};
+    if(in.intData==null||in.intData.length<2) in.intData=new int[] {0,-60};
   }
   public boolean isTreeLeaf(Block tb) {
-    return tb.type==pc.leaf;
+    return tb.type==this;
   }
   public void testCount(Block in,Block tb) {
     if(tb==null) {
       if(in.intData[1]<1) in.intData[1]=1;
-    }else if(isTreeLeaf(tb)&&tb.intData[0]>in.intData[1]) {
+    }else if(isTreeLeaf(tb)) {
       int ti=tb.intData[0]-1;
       if(ti>in.intData[1]) in.intData[1]=ti;
     }
@@ -91,15 +91,16 @@ public class TreeLeaf extends MetaBlock{
     updater=(in,x,y)-> {
       lightUpdater.update(in,x,y);
       int[] array=in.intData;
-      // int pointer=pc.pw.data.tick%2;
       World0001 world=pc.pw;
-      // if(pointer==0) {
-      if(array[1]==-1) {
-        array[1]=0;
-        testCount(in,world.getBlock(x,y+1));
-        testCount(in,world.getBlock(x,y-1));
-        testCount(in,world.getBlock(x+1,y));
-        testCount(in,world.getBlock(x-1,y));
+      if(array[1]<0) {
+        array[1]++;
+        if(array[1]==0) {
+          // array[1]=0;
+          testCount(in,world.getBlock(x,y+1));
+          testCount(in,world.getBlock(x,y-1));
+          testCount(in,world.getBlock(x+1,y));
+          testCount(in,world.getBlock(x-1,y));
+        }
       }else {
         array[0]=array[1];
         array[1]=-1;
@@ -107,16 +108,8 @@ public class TreeLeaf extends MetaBlock{
         if(tb!=null) {
           if(tb.type==pc.log) array[0]=array[1]=maxLogCount;
           else if(array[0]<=0) world.destroyBlock(in,x,y);
-          // else if(array[0]>maxLogCount) array[0]=maxLogCount;
         }
       }
-      // }else if(pointer==1) {
-      //   in.intData[1]=0;
-      //   testCount(in,world.getBlock(x,y+1));
-      //   testCount(in,world.getBlock(x,y-1));
-      //   testCount(in,world.getBlock(x+1,y));
-      //   testCount(in,world.getBlock(x-1,y));
-      // }
     };
     displayUpdater=(in,x,y)-> {
       World0001 world=in.type.pc.pw;
