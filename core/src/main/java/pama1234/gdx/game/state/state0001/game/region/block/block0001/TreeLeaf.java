8ab25197ed1d99e022a9cf6be673ce8f6ded7840
pama1234 <62501730+pama1234@users.noreply.github.com>
2023-02-12 10:55:45 +0800

    update

--- core/src/main/java/pama1234/gdx/game/state/state0001/game/region/block/block0001/TreeLeaf.java
+++ core/src/main/java/pama1234/gdx/game/state/state0001/game/region/block/block0001/TreeLeaf.java
@@ -74,14 +74,13 @@ public class TreeLeaf extends MetaBlock{
   }
   @Override
   public void initBlock(Block in) {
-    if(in.intData==null||in.intData.length<3) in.intData=new int[] {0,0,0};
+    if(in.intData==null||in.intData.length<2) in.intData=new int[] {0,-60};
   }
   public boolean isTreeLeaf(Block tb) {
     return tb.type==this;
   }
   public void testCount(Block in,Block tb) {
     if(tb==null) {
-      // if(in.intData[1]<maxLogCount)
       in.intData[1]=maxLogCount;
     }else if(isTreeLeaf(tb)) {
       int ti=tb.intData[0]-1;
@@ -93,26 +92,26 @@ public class TreeLeaf extends MetaBlock{
       lightUpdater.update(in,x,y);
       int[] array=in.intData;
       World0001 world=pc.pw;
-      if(array[2]>=0) array[2]=(array[2]+1)%3;
-      else array[2]++;
-      if(array[2]==0) {
-        // if(array[2]==0) {
-        // array[1]=0;
-        testCount(in,world.getBlock(x,y+1));
-        testCount(in,world.getBlock(x,y-1));
-        testCount(in,world.getBlock(x+1,y));
-        testCount(in,world.getBlock(x-1,y));
-        // }
-      }else if(array[2]==1) {
-        array[0]=array[1];
-        array[1]=0;
-        array[2]=-1;
+      if(array[1]<0) {
+        array[1]++;
+        if(array[1]>=-1) {
+          int ti=array[1];
+          array[1]=0;
+          // array[1]=0;
+          testCount(in,world.getBlock(x,y+1));
+          testCount(in,world.getBlock(x,y-1));
+          testCount(in,world.getBlock(x+1,y));
+          testCount(in,world.getBlock(x-1,y));
+          if(array[1]==0) array[1]=ti+1;
+        }
       }else {
+        array[0]=array[1];
+        array[1]=-1;
         Block tb=world.getBlock(x,y+1);
         if(tb!=null) {
           if(tb.type==pc.log) array[0]=array[1]=maxLogCount;
           else if(array[0]<=0) world.destroyBlock(in,x,y);
-        }else array[1]=maxLogCount;
+        }
       }
     };
     displayUpdater=(in,x,y)-> {
--- core/src/main/java/pama1234/gdx/game/ui/generator/UiGenerator.java
+++ core/src/main/java/pama1234/gdx/game/ui/generator/UiGenerator.java
@@ -141,6 +141,12 @@ public class UiGenerator{
       new TextButtonCam<T>(p,true,()->true,self-> {},self-> {},self-> {
         p.state(State0001.Debug);
       },self->self.text="别按这个（Debug视图）",()->18,()->0,()->160),
+      new TextButtonCam<T>(p,true,()->true,self-> {},self-> {},self-> {
+        //TODO
+      },self->self.text="使用陀螺仪：否",()->18,()->0,()->180),
+      new TextButtonCam<T>(p,true,()->true,self-> {},self-> {},self-> {
+        //TODO
+      },self->self.text="使用加速计：否",()->18,()->0,()->200),
     };
   }
   public static <T extends Screen0011> TextButton<?>[] genButtons_0008(T p) {
