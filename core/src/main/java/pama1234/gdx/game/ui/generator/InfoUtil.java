12c903ab88649d5ec69c42d275d7722b6ad25085
pama1234 <62501730+pama1234@users.noreply.github.com>
2023-02-16 11:24:19 +0800

    update

--- core/src/main/java/pama1234/gdx/game/ui/generator/InfoUtil.java
+++ core/src/main/java/pama1234/gdx/game/ui/generator/InfoUtil.java
@@ -11,7 +11,25 @@ import pama1234.gdx.game.app.Screen0011;
 import pama1234.gdx.game.state.state0001.game.KryoUtil;
 
 public class InfoUtil{
-  public static final String[] errorText=new String[] {"未获取线上信息"};
+  public static final String[] errorText=new String[] {
+    "以下是非联网状态下的古老公告信息：",
+    "咳咳，很明显你是一个新玩家！",
+    "你应该已经注意到了与应用图标（某二次元少女）和十分霸气的名字（空想世界 1）和与之十分不符的UI和画质",
+    "并且心中也应该有了某种不祥的预感（比如：这不会只是又一个itch.io上一捞一大把的像素独立游戏吧）",
+    "虽然但是，你的预感是十分准确的，咳咳，咳咳咳咳咳。。。。",
+    "---以下是游戏说明---",
+    "电脑端使用WASD或↑←↓→箭头按钮控制角色移动，空格控制角色跳跃，E打开或关闭背包",
+    "鼠标左键破坏方块，鼠标右键替换方块或使用工作站，鼠标左键双击攻击生物（需空手或持有具备攻击能力的武器）",
+    "鼠标左键拿起或放下物品栏内物品，鼠标右键或滚轮更改快捷栏",
+    "安卓端请单指拖拽或双指拖拽+缩放调节视角位置，电脑端使用+和-按键",
+    "工作台使用教程：最左侧的按钮，Step是每次只进行一次合成，要取走物品才进行下一次，On是连续合成，Off是关闭，",
+    "然后-和+是调节合成表的，有多个相同输入物品的合成表，因此需要调节合成表才能合成特定物品（比如石剑）",
+    "---以下是其他东东---",
+    "“我准备使用libgdx和其他一些东东做一个游戏！”",
+    "目前依然在原型测试阶段（而不是内测或公测或正式发布），欢迎加QQ群 589219461。",
+    "各位可以将觉得需要实现的功能提出来，我会排序后一一实现。",
+    "B站up主：“pama1234_2” 蓝色头像 id：646050693"
+  };
   public static FileHandle dataLocation=Gdx.files.local("/data/cache/net/info");
   public static FileHandle infoDataLocation=Gdx.files.local("/data/cache/net/info/data.bin");
   public static FileHandle infoSourceLocation=Gdx.files.local("/data/cache/net/info/source.bin");
@@ -101,13 +119,12 @@ public class InfoUtil{
     "H关闭提示，改粒子数据需要改动源代码才行",
     "pama1234出品，谢谢观看",
     "————在很久之后，他终于把半自制的3D框架写好啦"
-  },
-    info0003=new String[] {
-      "欢迎！以下是公告",
-      "请单指或双指拖拽或使用鼠标右键和滚轮调节视角位置。",
-      "这是一个空白的游戏框架，可以作为各位写游戏的模板。",
-      "目前仍在开发阶段，欢迎加QQ群 589219461。"
-    };
+  },info0003=new String[] {
+    "欢迎！以下是公告",
+    "请单指或双指拖拽或使用鼠标右键和滚轮调节视角位置。",
+    "这是一个空白的游戏框架，可以作为各位写游戏的模板。",
+    "目前仍在开发阶段，欢迎加QQ群 589219461。"
+  };
   public static class InfoSource{
     public String url;
     public String start,end,versionDeclare;
