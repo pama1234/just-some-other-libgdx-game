8e8827b007c60f1921da57ea2b1b72b62e6a4020
Pama1234 <pama1234@163.com>
2023-02-17 16:06:55 +0800

    update

--- core/src/main/java/pama1234/gdx/game/state/state0001/game/net/ClientCore.java
+++ core/src/main/java/pama1234/gdx/game/state/state0001/game/net/ClientCore.java
@@ -9,10 +9,31 @@ public class ClientCore{
   public World0001 world;
   //---
   public SocketData socketData;
+  public ClientRead clientRead;
+  public ClientWrite clientWrite;
+  public Thread connectThread;
   public ClientCore(Game game,World0001 world,SocketData socketData) {
     this.game=game;
     this.world=world;
     this.socketData=socketData;
+    connectThread=new Thread(()-> {
+      while(!socketData.s.isConnected()) game.p.sleep(200);
+      //TODO
+      (clientRead=new ClientRead(this)).start();
+      (clientWrite=new ClientWrite(this)).start();
+    },"ConnectThread");
   }
   public void start() {}
+  public static class ClientRead extends Thread{
+    public ClientCore p;
+    public ClientRead(ClientCore p) {
+      this.p=p;
+    }
+  }
+  public static class ClientWrite extends Thread{
+    public ClientCore p;
+    public ClientWrite(ClientCore p) {
+      this.p=p;
+    }
+  }
 }
--- core/src/main/java/pama1234/gdx/game/state/state0001/game/net/ServerCore.java
+++ core/src/main/java/pama1234/gdx/game/state/state0001/game/net/ServerCore.java
@@ -1,18 +1,55 @@
 package pama1234.gdx.game.state.state0001.game.net;
 
+import pama1234.game.app.server.server0002.net.SocketData;
 import pama1234.gdx.game.state.state0001.Game;
 import pama1234.gdx.game.state.state0001.game.world.World0001;
 import pama1234.util.net.ServerSocketData;
+import pama1234.util.net.SocketWrapper;
+import pama1234.util.wrapper.Center;
 
 public class ServerCore{
   public Game game;
   public World0001 world;
   //---
   public ServerSocketData serverSocketData;
+  public Center<SocketData> socketCenter;
+  public Center<ServerRead> serverReadPool;
+  public Center<ServerWrite> serverWritePool;
+  public Thread acceptThread;
+  public boolean stop;
   public ServerCore(Game game,World0001 world,ServerSocketData serverSocketData) {
     this.game=game;
     this.world=world;
     this.serverSocketData=serverSocketData;
+    acceptThread=new Thread(()-> {
+      while(!stop) {
+        SocketData socketData=new SocketData(new SocketWrapper(serverSocketData.accept()));
+        socketCenter.add.add(socketData);
+        //---
+        ServerWrite serverWrite=new ServerWrite(this);
+        serverWrite.start();
+        serverWritePool.add.add(serverWrite);
+        //---
+        ServerRead serverRead=new ServerRead(this);
+        serverRead.start();
+        serverReadPool.add.add(serverRead);
+        // }
+      }
+    },"AcceptSocket");
   }
-  public void start() {}
+  public void start() {
+    acceptThread.start();
+  }
+  public static class ServerWrite extends Thread{
+    ServerCore p;
+    public ServerWrite(ServerCore p) {
+      this.p=p;
+    }
+  }
+  public static class ServerRead extends Thread{
+    ServerCore p;
+    public ServerRead(ServerCore p) {
+      this.p=p;
+    }
+  }
 }
