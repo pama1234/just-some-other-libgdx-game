c470d3530e6f5e1d8abe10463a8c110dc9ddbad2
Pama1234 <pama1234@163.com>
2023-02-21 19:47:14 +0800

    update

--- core/src/main/java/pama1234/gdx/util/info/MouseInfo.java
+++ core/src/main/java/pama1234/gdx/util/info/MouseInfo.java
@@ -7,11 +7,44 @@ import pama1234.math.Tools;
 
 public class MouseInfo{
   public UtilScreenCore p;
-  public boolean pressed,left,right,center;
-  public int startTime,button=-1;
+  /**
+   * 表示鼠标按键是否被按下
+   */
+  public boolean pressed;
+  /**
+   * 表示鼠标被按下的是哪个键
+   */
+  public boolean left,right,center;
+  /**
+   * （未实现）用于记录鼠标开始时的当前帧数
+   * 
+   * @see pama1234.gdx.util.app.UtilScreenCore#frameCount
+   */
+  public int startTime;
+  /**
+   * 被按下的键的int表示
+   * 
+   * @see com.badlogic.gdx.Input.Buttons
+   */
+  public int button=-1;
+  /**
+   * sx是start-x的简写，含义是鼠标被按下时所处的位置
+   * </p>
+   */
   public float sx,sy;
+  /**
+   * px是previous-x的简写，表示上一帧时鼠标的位置 x和y是当前这一帧的鼠标位置
+   */
   public float px,py,x,y;
+  /**
+   * dx是distance-x的简写，表示这一帧和上一帧的鼠标位置的数值差，可为负数
+   */
   public float dx,dy;
+  /**
+   * ox是未经过相机视角变化的当前鼠标位置
+   * 
+   * @see pama1234.gdx.util.app.UtilScreen2D#unproject(float,float)
+   */
   public int ox,oy;
   public MouseInfo(UtilScreenCore p) {
     this.p=p;
@@ -24,8 +57,6 @@ public class MouseInfo{
   public void putRaw(int x,int y) {
     Vector3 tv=p.unproject(ox=x,oy=y);
     put(tv.x,tv.y);
-    // System.out.println(dx+" "+x+" "+px);
-    // System.out.println(dy+" "+y+" "+py);
     dx=x-px;
     dy=y-py;
   }
--- core/src/main/java/pama1234/gdx/util/listener/EntityListener.java
+++ core/src/main/java/pama1234/gdx/util/listener/EntityListener.java
@@ -3,15 +3,55 @@ package pama1234.gdx.util.listener;
 import pama1234.gdx.util.info.MouseInfo;
 import pama1234.gdx.util.info.TouchInfo;
 import pama1234.util.listener.ServerEntityListener;
-
+/**
+ * 一个实体，指的是可监听各类事件（键盘鼠标，绘制刷新）的对象
+ */
 public interface EntityListener extends ServerEntityListener,InputListener{
+  /**
+   * 调用绘制方法，每帧调用一次，libgdx的OpenGL默认使用双缓冲区，也就是说每次绘制都应当清除之前的绘图，否则会导致画面闪烁
+   * 
+   * @see pama1234.gdx.util.app.UtilScreen#render(float)
+   * 
+   * @see pama1234.gdx.util.app.UtilScreen#display()
+   * @see pama1234.gdx.util.app.UtilScreen#doDraw()
+   * 
+   * @see pama1234.gdx.util.app.UtilScreen#background(int)
+   * @see pama1234.gdx.util.app.UtilScreen#backgroundColor(int)
+   */
   default void display() {}
+  /**
+   * 调用刷新方法，理论上每帧调用一次，实际不一定，“类似后端的业务逻辑”和绘制逻辑应当分开，一般来说，在update方法中不能调用和绘制相关的方法，但是display方法中可以进行update中才能进行的操作
+   * 
+   * @see pama1234.gdx.util.app.UtilScreen#render(float)
+   * 
+   * @see pama1234.gdx.util.app.UtilScreen#update()
+   * @see pama1234.gdx.util.app.UtilScreen#doUpdate()
+   * 
+   */
   default void update() {}
+  /**
+   * 此实体将被清理时被调用，一般在程序结束时被调用，安卓端的行为不明确
+   */
   default void dispose() {}
+  /**
+   * 可能会在实体被创建时调用，但是不一定
+   */
   default void init() {}
+  /**
+   * 在安卓端切后台时会被调用，电脑端目前基本无作用（除非更改lwjgl的配置）
+   */
   default void pause() {}
+  /**
+   * 在安卓端从后台切换到使用此框架的APP时会被调用，电脑端目前基本无作用（除非更改lwjgl的配置）
+   */
   default void resume() {}
+  /**
+   * 在电脑端的窗口发生移动时被调用，安卓端不会被调用
+   */
   default void frameMoved(int x,int y) {}
+  /**
+   * 在电脑端的窗口发生移动时会被调用，安卓端大概不会被调用
+   */
   default void frameResized(int w,int h) {}
   default void keyPressed(char key,int keyCode) {}
   default void keyReleased(char key,int keyCode) {}
