5571cdcf8a57146abeaf35daeca7e0e87dffc37f
pama1234 <62501730+pama1234@users.noreply.github.com>
2023-03-06 14:35:41 +0800

    update

--- game0002/core/src/main/java/pama1234/gdx/game/state/state0001/game/region/LoadAndSave.java
+++
@@ -1,6 +0,0 @@
-package pama1234.gdx.game.state.state0001.game.region;
-
-public interface LoadAndSave{
-  public void load();
-  public void save();
-}
--- game0002/core/src/main/java/pama1234/gdx/game/ui/generator/UiGenerator.java
+++ game0002/core/src/main/java/pama1234/gdx/game/ui/generator/UiGenerator.java
@@ -2,114 +2,12 @@ package pama1234.gdx.game.ui.generator;
 
 import com.badlogic.gdx.Input;
 
-import pama1234.gdx.game.app.Screen0011;
 import pama1234.gdx.game.app.app0002.Screen0006;
-import pama1234.gdx.game.asset.ImageAsset;
-import pama1234.gdx.game.state.state0001.Game;
-import pama1234.gdx.game.state.state0001.State0001;
-import pama1234.gdx.game.state.state0001.game.region.block.Block;
 import pama1234.gdx.game.ui.util.Button;
 import pama1234.gdx.game.ui.util.TextButton;
-import pama1234.gdx.game.ui.util.TextButtonCam;
-import pama1234.gdx.game.ui.util.TextureButton;
 import pama1234.gdx.util.app.ScreenCore3D;
 
 public class UiGenerator{
-  public static <T extends Screen0011> TextButtonCam<?>[] genButtons_0010(Screen0011 p,Block in) {
-    return new TextButtonCam[] {
-      new TextButtonCam<Screen0011>(p,true,()->true,self-> {},self-> {},self-> {
-        //---
-      },self->self.text="背包显示方式：同心圆（未实现）",()->18,()->in.intData[2]-20,()->in.intData[3]),
-      new TextButtonCam<Screen0011>(p,true,()->true,self-> {},self-> {},self-> {
-        in.type.pc.pw.timeF=0;
-        in.type.pc.pw.data.tick=0;
-        in.type.pc.pw.data.time=12000;
-      },self->self.text="重置世界时间",()->18,()->in.intData[2]-20,()->in.intData[3]+20),
-    };
-  }
-  public static <T extends Screen0011> TextButtonCam<?>[] genButtons_0009(Screen0011 p,Block in) {
-    String[] text0001=new String[] {"On  ","Off ","Step"};
-    return new TextButtonCam[] {
-      new TextButtonCam<Screen0011>(p,true,()->true,self-> {},self-> {},self-> {
-        in.intData[1]=(in.intData[1]+1)%3;
-        self.updateText();
-      },self->self.text=text0001[in.intData[1]],()->18,()->in.intData[2]-5,()->in.intData[3]),
-      new TextButtonCam<Screen0011>(p,true,()->true,self-> {},self-> {},self-> {
-        in.intData[0]-=1;
-        if(in.intData[0]<0) in.intData[0]+=in.type.intData[0];
-      },self->self.text="-",()->18,()->in.intData[2]+45,()->in.intData[3]),
-      new TextButtonCam<Screen0011>(p,true,()->true,self-> {},self-> {},self-> {
-        in.intData[0]+=1;
-        if(in.intData[0]>=in.type.intData[0]) in.intData[0]-=in.type.intData[0];
-      },self->self.text="+",()->18,()->in.intData[2]+71,()->in.intData[3]),
-    };
-  }
-  public static <T extends Screen0011> TextButton<?>[] genButtons_0007(T p,Game pg) {
-    return new TextButton[] {
-      new TextButton<T>(p,true,()->true,self-> {},self-> {},self-> {
-        pg.androidRightMouseButton=!pg.androidRightMouseButton;
-        self.updateText();
-      },self->self.text=pg.androidRightMouseButton?"mR":"mL",p::getButtonUnitLength,()->p.width-p.bu*4f,()->p.height-p.bu*1.5f,()->p.bu-p.pus,false),
-      // new TextButton<T>(p,true,()->true,self-> {},self-> {
-      //   pg.world().yourself.ctrl.shift(!pg.world().yourself.ctrl.shift);
-      //   self.updateText();
-      // },self-> {},
-      //   self->self.text=pg.world().yourself.ctrl.shift?"S":"s","s",p::getButtonUnitLength,()->p.width-p.bu*2.5f,()->p.height-p.bu*1.5f,()->p.bu-p.pus,true),
-      new TextButton<T>(p,true,()->true,self-> {},self-> {
-        p.inputProcessor.keyDown(Input.Keys.S);
-      },self-> {
-        p.inputProcessor.keyUp(Input.Keys.S);
-      },self->self.text="↓",p::getButtonUnitLength,()->p.width-p.bu*2.5f,()->p.height-p.bu*1.5f,()->p.bu-p.pus,false),
-      new TextButton<T>(p,true,()->true,self-> {},self-> {
-        p.inputProcessor.keyDown(Input.Keys.A);
-      },self-> {
-        p.inputProcessor.keyUp(Input.Keys.A);
-      },self->self.text="← ",p::getButtonUnitLength,()->p.bu*1.5f,()->p.height-p.bu*1.5f,()->p.bu-p.pus,false),
-      new TextButton<T>(p,true,()->true,self-> {},self-> {
-        p.inputProcessor.keyDown(Input.Keys.D);
-      },self-> {
-        p.inputProcessor.keyUp(Input.Keys.D);
-      },self->self.text=" →",p::getButtonUnitLength,()->p.bu*3f,()->p.height-p.bu*1.5f,()->p.bu-p.pus,false),
-      //--------------------------------------------------------------------
-      new TextButton<T>(p,true,()->true,self-> {},self-> {
-        p.inputProcessor.keyDown(Input.Keys.SPACE);
-      },self-> {
-        p.inputProcessor.keyUp(Input.Keys.SPACE);
-      },self->self.text="↑",p::getButtonUnitLength,()->p.width-p.bu*2.5f,()->p.height-p.bu*2.5f,()->p.bu-p.pus,false),
-      new TextButton<T>(p,true,()->true,self-> {},self-> {},self-> {
-        p.inputProcessor.scrolled(0,1);
-      },self->self.text="+ ",p::getButtonUnitLength,()->p.width-p.bu*3.5f,()->p.bu*0.5f,()->p.bu-p.pus,false),
-      new TextButton<T>(p,true,()->true,self-> {},self-> {},self-> {
-        p.inputProcessor.scrolled(0,-1);
-      },self->self.text=" -",p::getButtonUnitLength,()->p.width-p.bu*2f,()->p.bu*0.5f,()->p.bu-p.pus,false),
-    };
-  }
-  public static <T extends Screen0011> TextButton<?>[] genButtons_0008(T p) {
-    return new TextButton[] {
-      new TextButton<T>(p,true,()->true,self-> {
-        p.cam2d.scale.des+=1/32f;
-        p.cam2d.testScale();
-      },self-> {},self-> {},self->self.text="+",p::getButtonUnitLength,()->p.bu*0.5f,()->p.bu*1.5f+p.pus,()->p.bu-p.pus,false),
-      new TextButton<T>(p,true,()->true,self-> {
-        p.cam2d.scale.des-=1/32f;
-        p.cam2d.testScale();
-      },self-> {},self-> {},self->self.text="-",p::getButtonUnitLength,()->p.bu*0.5f,()->p.bu*2.5f+p.pus,()->p.bu-p.pus,false),
-    };
-  }
-  public static <T extends Screen0011> Button<?>[] genButtons_0005(T p) {
-    return new Button[] {
-      new TextureButton<T>(p,true,()->true,self-> {},self-> {},self-> {
-        p.state(State0001.StartMenu);
-      },()->ImageAsset.exit,p::getButtonUnitLength,()->p.bu*0.2f,()->p.bu*0.2f,()->p.bu,()->p.bu),
-    };
-  }
-  public static <T extends Screen0011> Button<?>[] genButtons_0004(T p) {
-    return new Button[] {
-      new TextButton<T>(p,true,()->true,self-> {},self-> {},self-> {
-        p.state(State0001.StartMenu);
-      },self->self.text="返回",p::getButtonUnitLength,()->p.bu*0.5f,()->p.bu*0.5f),
-    };
-  }
   public static <T extends Screen0006> Button<?>[] genButtons_0002(T p) {
     return new Button[] {
       new TextButton<T>(p,true,()->true,self-> {},self-> {},self-> {
