8cc96180019f5e490a8075e362b1b3be542b0ad2
DESKTOP-HVUU026\WIN10 <lizerun@galasports.com>
2023-05-16 17:01:53 +0800

    update

--- framework0001/framework0001-localization/src/main/java/pama1234/util/localization/LocalBundle.java
+++ framework0001/framework0001-localization/src/main/java/pama1234/util/localization/LocalBundle.java
@@ -1,8 +1,23 @@
 package pama1234.util.localization;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
 import com.esotericsoftware.kryo.serializers.TaggedFieldSerializer.Tag;
 
 public class LocalBundle{
   @Tag(0)
   public String[] data;
+  public void loadFrom(Yaml yaml,String yamlData,String[] name) {//TODO
+    yaml.loadAs(yamlData,null);
+  }
+  public String getYaml(Yaml yaml,String[] name) {
+    return getYaml(yaml,name,new HashMap<>(name.length));
+  }
+  public String getYaml(Yaml yaml,String[] name,Map<String,String> cache) {
+    for(int i=0;i<name.length;i++) cache.put(name[i],data[i]);
+    return yaml.dumpAsMap(cache);
+  }
 }
--- framework0001/framework0001-localization/src/main/java/pama1234/util/localization/MainTest.java
+++ framework0001/framework0001-localization/src/main/java/pama1234/util/localization/MainTest.java
@@ -1,23 +1,37 @@
 package pama1234.util.localization;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.HashMap;
+
+import org.yaml.snakeyaml.Yaml;
 
 public class MainTest{
+  // public static void main(String[] args) {
+  //   //default locale
+  //   ResourceBundle bundle=ResourceBundle.getBundle("ApplicationMessages");
+  //   //Get ResourceBundle with Locale that are already defined
+  //   ResourceBundle bundleFR=ResourceBundle.getBundle("ApplicationMessages",Locale.FRANCE);
+  //   //Get resource bundle when Locale needs to be created
+  //   ResourceBundle bundleSWE=ResourceBundle.getBundle("ApplicationMessages",new Locale("sv","SE"));
+  //   //lets print some messages
+  //   printMessages(bundle);
+  //   printMessages(bundleFR);
+  //   printMessages(bundleSWE);
+  // }
+  // private static void printMessages(ResourceBundle bundle) {
+  //   System.out.println(bundle.getString("CountryName"));
+  //   System.out.println(bundle.getString("CurrencyCode"));
+  // }
   public static void main(String[] args) {
-    //default locale
-    ResourceBundle bundle=ResourceBundle.getBundle("ApplicationMessages");
-    //Get ResourceBundle with Locale that are already defined
-    ResourceBundle bundleFR=ResourceBundle.getBundle("ApplicationMessages",Locale.FRANCE);
-    //Get resource bundle when Locale needs to be created
-    ResourceBundle bundleSWE=ResourceBundle.getBundle("ApplicationMessages",new Locale("sv","SE"));
-    //lets print some messages
-    printMessages(bundle);
-    printMessages(bundleFR);
-    printMessages(bundleSWE);
-  }
-  private static void printMessages(ResourceBundle bundle) {
-    System.out.println(bundle.getString("CountryName"));
-    System.out.println(bundle.getString("CurrencyCode"));
+    Yaml yaml=new Yaml();
+    // System.out.println(yaml.dump(new String[] {"同意","退出"}));
+    HashMap<String,String> data=new HashMap<String,String>();
+    data.put("agree","同意");
+    data.put("nop","打咩");
+    // Object data=new Object() {
+    //   public String firstName="John";
+    //   public String lastName="Doe";
+    //   public int age=20;
+    // };
+    System.out.println(yaml.dumpAsMap(data));
   }
 }
