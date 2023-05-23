99544f92ad09fc51fbbd8c19cb26644b3c852241
DESKTOP-HVUU026\WIN10 <lizerun@galasports.com>
2023-05-23 17:09:46 +0800

    update

--- framework0001/framework0001-protobuf/src/main/java/pama1234/util/protobuf/TestApp.java
+++ framework0001/framework0001-protobuf/src/main/java/pama1234/util/protobuf/TestApp.java
@@ -5,17 +5,19 @@ import java.util.Arrays;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 
+import pama1234.util.protobuf.SettingsDataProto.SettingsData;
+
 public class TestApp{
   public static void main(String[] args) {
     // Kryo kryo=new Kryo();
     //初始化数据
-    DemoProto.Demo.Builder demo=DemoProto.Demo.newBuilder();
-    demo.setId(1)
+    DemoProto.Demo.Builder demoBuilder=DemoProto.Demo.newBuilder();
+    demoBuilder.setId(1)
       .setCode("001")
       .setName("张三")
       .build();
     //序列化
-    DemoProto.Demo build=demo.build();
+    DemoProto.Demo build=demoBuilder.build();
     //转换成字节数组
     byte[] s=build.toByteArray();
     System.out.println("protobuf数据bytes[]:"+Arrays.toString(s));
@@ -32,5 +34,21 @@ public class TestApp{
     }
     System.out.println("Json格式化结果:\n"+jsonObject);
     System.out.println("Json格式化数据大小: "+jsonObject.getBytes().length);
+    //---
+    var dataBuilder=SettingsDataProto.SettingsData.newBuilder();
+    dataBuilder.setVolume(1).setGyroscopeSensitivity(1).setAccelerometerSensitivity(1).setGConst(9.81f);
+    SettingsData data=dataBuilder.build();
+    byte[] byteArray=data.toByteArray();
+    System.out.println("protobuf数据bytes[]:"+Arrays.toString(byteArray));
+    System.out.println("protobuf序列化大小: "+byteArray.length);
+    System.out.println(byteArray);
+    try {
+      SettingsData data1=SettingsData.parseFrom(byteArray);
+      jsonObject=JsonFormat.printer().print(data1);
+      System.out.println("Json格式化结果:\n"+jsonObject);
+      System.out.println("Json格式化数据大小: "+jsonObject.getBytes().length);
+    }catch(InvalidProtocolBufferException e) {
+      e.printStackTrace();
+    }
   }
 }
