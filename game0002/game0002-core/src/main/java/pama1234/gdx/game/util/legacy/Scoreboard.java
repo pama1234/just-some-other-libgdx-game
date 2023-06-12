46e2ab46328e81e573791e4f79d8fb741d6dfaa2
DESKTOP-HVUU026\WIN10 <lizerun@galasports.com>
2023-06-12 16:18:15 +0800

    update

--- game0002/game0002-core/src/main/java/pama1234/gdx/game/util/legacy/Scoreboard.java
+++ game0002/game0002-core/src/main/java/pama1234/gdx/game/util/legacy/Scoreboard.java
@@ -68,7 +68,7 @@ public class Scoreboard extends TextBoard{
   public void display() {
     if(parent.index==1) {
       p.withScreen();
-      p.image(g.texture,(point.pos.x-g.width())/2,p.pu/4f);
+      p.image(g.texture,(p.width-g.width()*p.pus)/2,p.pu/4f,g.width()*p.pus,g.height()*p.pus);
       // p.image(g.texture,point.pos.x-g.width()/2,point.pos.y-g.height()/2);
       p.withCam();
     }
