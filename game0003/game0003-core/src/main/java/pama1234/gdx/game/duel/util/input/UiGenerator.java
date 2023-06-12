7c89262bc29a2b7a2562faa159ed1a93685c88a7
DESKTOP-HVUU026\WIN10 <lizerun@galasports.com>
2023-06-12 15:28:13 +0800

    update

--- game0003/game0003-core/src/main/java/pama1234/gdx/game/duel/util/input/UiGenerator.java
+++ game0003/game0003-core/src/main/java/pama1234/gdx/game/duel/util/input/UiGenerator.java
@@ -1,9 +1,17 @@
 package pama1234.gdx.game.duel.util.input;
 
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.utils.FocusListener;
 
 import pama1234.gdx.game.duel.Duel;
+import pama1234.gdx.game.duel.util.skin.SkinData;
+import pama1234.gdx.game.ui.CodeTextFieldStyle;
+import pama1234.gdx.game.ui.util.NormalOnscreenKeyboard;
+import pama1234.gdx.game.ui.util.TextArea;
 import pama1234.gdx.game.ui.util.TextButton;
+import pama1234.gdx.game.ui.util.TextField;
+import pama1234.math.geometry.RectF;
 
 public class UiGenerator{
   public static TextButton<?>[] genButtons_0001(Duel p) {
@@ -28,5 +36,29 @@ public class UiGenerator{
         self.updateText();
       },self-> {},self->self.text=p.state==p.stateCenter.game?"设置":"游戏",p::getButtonUnitLength,()->p.width-p.bu*3f,()->p.bu*0.5f,()->p.bu-p.pus,false),
     };
+  }
+  public static TextArea[] genUi_0001(Duel p) {
+    TextArea[] out=new TextArea[] {new TextArea(Duel.localization.yaml.dumpAsMap(p.config.skin.data),new CodeTextFieldStyle(p),
+      new RectF(()->20,()->20,()->600,()->300),
+      ()->1)};
+    // ()->p.pus)};
+    out[0].setMessageText("皮肤配置文件");
+    out[0].addListener(new FocusListener() {
+      @Override
+      public void keyboardFocusChanged(FocusEvent event,Actor actor,boolean focused) {
+        if(!focused) {
+          // String newLangType=out[0].getText().trim();
+          try {
+            p.config.skin.data=Duel.localization.yaml.load(out[0].getText());
+            p.skin=SkinData.fromData(p.config.skin);
+          }catch(RuntimeException e) {
+            out[0].setText(Duel.localization.yaml.dumpAsMap(p.config.skin.data));
+          }
+        }
+      }
+    });
+    // System.out.println(out[0].getListeners().size);
+    for(TextField e:out) e.setOnscreenKeyboard(new NormalOnscreenKeyboard());
+    return out;
   }
 }
--- game0003/game0003-core/src/main/java/pama1234/gdx/game/state/state0002/Settings.java
+++ game0003/game0003-core/src/main/java/pama1234/gdx/game/state/state0002/Settings.java
@@ -2,11 +2,26 @@ package pama1234.gdx.game.state.state0002;
 
 import pama1234.gdx.game.duel.Duel;
 import pama1234.gdx.game.duel.State0002Util.StateEntity0002;
+import pama1234.gdx.game.duel.util.input.UiGenerator;
+import pama1234.gdx.game.ui.util.TextArea;
 import pama1234.gdx.game.ui.util.TextField;
 
 public class Settings extends StateEntity0002{
-  public TextField[] screenTextFields;
+  public TextArea[] textFields;
   public Settings(Duel p,int id) {
     super(p,id);
+    init();
+  }
+  @Override
+  public void init() {
+    textFields=UiGenerator.genUi_0001(p);
+  }
+  @Override
+  public void from(StateEntity0002 in) {
+    for(TextField e:textFields) p.camStage.addActor(e);
+  }
+  @Override
+  public void to(StateEntity0002 in) {
+    for(TextField e:textFields) e.remove();
   }
 }
