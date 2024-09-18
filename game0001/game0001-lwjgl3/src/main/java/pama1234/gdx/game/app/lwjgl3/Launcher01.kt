ca26bf5143d947fd87c9f3ff712328a843a11856
pama1234 <pama1234@163.com>
2024-09-18 22:20:15 +0800

    draft

--- game0001/game0001-lwjgl3/src/main/java/pama1234/gdx/game/app/lwjgl3/Launcher01.kt
+++ game0001/game0001-lwjgl3/src/main/java/pama1234/gdx/game/app/lwjgl3/Launcher01.kt
@@ -1,24 +1,26 @@
-package pama1234.gdx.game.app.lwjgl3;
+package pama1234.gdx.game.app.lwjgl3
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import pama1234.gdx.launcher.MainApp
+import pama1234.util.gdx.lwjgl.Lwjgl3ApplicationPama
+import pama1234.util.gdx.lwjgl.UtilLauncher
 
-import pama1234.gdx.launcher.MainApp;
-import pama1234.util.gdx.lwjgl.Lwjgl3ApplicationPama;
-import pama1234.util.gdx.lwjgl.UtilLauncher;
+/** Launches the desktop (LWJGL3) application.  */
+object Launcher01 : UtilLauncher() {
+    @kotlin.jvm.JvmStatic
+    fun main(args: Array<String>) {
+        createApplication()
+    }
 
-/** Launches the desktop (LWJGL3) application. */
-public class Launcher01 extends UtilLauncher{
-  public static void main(String[] args) {
-    createApplication();
-  }
-  public static Lwjgl3Application createApplication() {
-    Lwjgl3ApplicationPama.init();
-    MainApp app=new MainApp();
-    return new Lwjgl3Application(app,getConfiguration(app));
-  }
-  public static Lwjgl3ApplicationConfiguration getConfiguration(MainApp app) {
-    Lwjgl3ApplicationConfiguration conf=getDefaultConfiguration(app);
-    return conf;
-  }
+    fun createApplication(): Lwjgl3Application {
+        Lwjgl3ApplicationPama.init()
+        val app = MainApp()
+        return Lwjgl3Application(app, getConfiguration(app))
+    }
+
+    fun getConfiguration(app: MainApp?): Lwjgl3ApplicationConfiguration {
+        val conf = getDefaultConfiguration(app)
+        return conf
+    }
 }
